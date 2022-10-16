c34300083ab4df92d7d841632bd7ae5c1de9b0fa
Thanh <hieuthanh4a2@gmail.com>
2022-10-16 23:48:13 +0700

    my commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-  printf("He lô world");
+int main()
+{
+	printf("He lô world");
 	cout << "Xin chaoooo0000!";
 	cout << "Xin chao!";
 	return 0;
 }
-
